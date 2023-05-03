cfb21f391db3016af6f1a9e3d5ebb3d40c89495d
3SSI3 <starsm004@gmail.com>
2023-05-03 04:05:24 +0300

    Done :hammer_and_wrench:

---
+++ api/v1/views/index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module implement rule that returns the status of the application
+"""
+
+import models from storage
+import jsonify from flask
+import Amenity from models.amenity
+import City from models.city
+import Place from models.place
+import Review from models.review
+import State from models.state
+import User from models.user
+import app_views from api.v1.views
+
+@app_views.route("/status", strict_slashes=False)
+def view_status():
+    """View function that return a json message"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def view_stats():
+    """Veiw function that retrieves the number of each object by type"""
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
